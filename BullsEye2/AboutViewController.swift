e947f66bd48b1aaa2e4984f9ca79046118eb97c3
Michelle Guthrie-Harrigan <mguthr5@live.spcollege.edu>
2021-08-24 00:15:51 -0400

    Second try

---
+++ BullsEye2/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Michelle Guthrie-Harrigan on 8/22/21.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet var webView: WKWebView!
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(
+            forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+}
